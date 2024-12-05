cb68482a7f5877b502649fad77e55475e3e3ffb2
kaziabulhasib <hasibkazi420@gmail.com>
2024-12-05 11:06:21 +0530

    Navbar:second part -setactivesection & scrolltosection

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,22 @@
-import React from "react";
-
+"use client";
+import React, { useCallback, useEffect, useState } from "react";
+import cn from "classnames";
 const NavBar = () => {
+  const [activeSection, setActiveSection] = useState("home");
+  useEffect(() => {
+    setActiveSection("home");
+  }, []);
+
+  const scrollToSection = useCallback((sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  }, []);
   return (
-    <div>
+    <div className='fixed top-12 right-6 mx-auto flex flex-col gap-2.5 items-end z-50'>
       <button className='bg-background card-shadow p-3 md:hidden rounded'>
         <img
           className='block dark:hidden'
@@ -15,6 +29,20 @@ const NavBar = () => {
           alt='menu icon'
         />
       </button>
+      <nav>
+        <ul>
+          <li>
+            <div
+              className={cn("rounded p-1", { "bg-primary text-white": true })}
+              onClick={() => {
+                setActiveSection("home");
+                scrollToSection("home");
+              }}>
+              Home
+            </div>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
