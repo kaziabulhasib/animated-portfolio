f2a9bce89f17c7ac3c2ff35d9e56a1fd38075e81
kaziabulhasib <hasibkazi420@gmail.com>
2025-01-21 12:10:50 +0530

    projects section seond part

--- app/components/Projects/Project.tsx
+++ app/components/Projects/Project.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 
-type Props = {};
+type Props = {
+  thumbnail: string;
+  title: string;
+  link: {
+    url: string;
+    label: string;
+  };
+  description: string;
+  languageIcons: string[];
+};
 
-function Project({}: Props) {
-  return <div>Project</div>;
+function Project({
+  thumbnail,
+  title,
+  link,
+  description,
+  languageIcons,
+}: Props) {
+  return (
+    <div className='card flex flex-col  items-stretch w-full max-w-[428.4px] p-5 md:p-[18px] gap-[30px]'>
+      Project
+    </div>
+  );
 }
 
 export default Project;
--- app/components/Projects/Projects.tsx
+++ app/components/Projects/Projects.tsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import SectionContainer from "../Section/SectionContainer";
+import SectionHeader from "../Section/SectionHeader";
+import projects from "@/data/projects.json";
+import Project from "./Project";
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <SectionContainer id='projects'>
+      <div className='section-contents mx-6 md:mx-16'>
+        <SectionHeader plainText='😎 some of my' highlightText='Best Works' />
+        <div className='lg:w-full grid grid-cols-1 lg:grid-cols-2 gap-10'>
+          {projects.map((project, id) => (
+            <Project
+              key={id}
+              thumbnail={project.thumbnail}
+              title={project.title}
+              link={project.link}
+              description={project.description}
+              languageIcons={project.languageIcons}
+            />
+          ))}
+        </div>
+      </div>
+    </SectionContainer>
+  );
 };
 
 export default Projects;
