74a1f6c2739e6cb1251a327c37234fb05022f38c
kaziabulhasib <hasibkazi420@gmail.com>
2025-01-22 10:01:21 +0530

    project card second part

--- app/components/Projects/Project.tsx
+++ app/components/Projects/Project.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {
@@ -20,7 +21,23 @@ function Project({
 }: Props) {
   return (
     <div className='card flex flex-col  items-stretch w-full max-w-[428.4px] p-5 md:p-[18px] gap-[30px]'>
-      Project
+      <Image
+        src={thumbnail}
+        alt={`Thumbnail for ${title}`}
+        width={392}
+        height={230}
+        className='w-full h-[230px] object-cover'
+      />
+      <div className='flex flex-col gap-[11px]'>
+        <div className='flex justify-between items-center'>
+          <h3 className='font-semibold text-xl md:text-[22px]'>{title}</h3>
+          <a
+            href={link.url}
+            className='flex px-[5px] py-[3px] gap-1 bg-[#b9b9b9] bg-opacity-[24%] rounded text-sm'>
+            <span className='hidden md:block'>{link.label}</span>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co", // Specify the domain
+        pathname: "/**", // Allow all paths under the domain
+      },
+    ],
+  },
 };
 
 export default nextConfig;
